7eff451ba74e3fb0ad4e2e4d61f1c1480d4d26df
rosales <rosalesprime77777@gmail.com>
2026-07-01 21:15:03 -0500

    versión 0.0.1

---
+++ tienda/her.py
@@ -0,0 +1,30 @@
+def pedir_info_entero(texto):
+    while True:
+        try:
+            cuanto_agregas = int(input(texto))
+            if cuanto_agregas <= 0:
+                print("solo numeros enteros")
+                continue
+            break
+        except ValueError:
+            print("solo numeros")
+    return cuanto_agregas
+    
+def pedir_info_flotante(texto):
+    while True:
+        try:
+            cuanto_agregas = float(input(texto))
+            if cuanto_agregas <= 0:
+                print("solo numeros enteros")
+                continue
+            break
+        except ValueError:
+            print("solo numeros")
+            
+    return cuanto_agregas
+
+
+
+
+
+                
